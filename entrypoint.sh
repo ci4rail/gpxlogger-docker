c0420d2d95b0b4d0c8337b140d9294c0e5b3b47d
FranziskaKnoedtel <81755573+FranziskaKnoedtel@users.noreply.github.com>
2022-02-16 13:56:38 +0100

    Change name to be also valid for windows filesystems

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -o pipefail  # don't hide errors within pipes
 
 
 while true; do
-    filename=track-$(date +%Y-%m-%d-%H:%M:%S).gpx
+    filename=track-$(date +%Y-%m-%d_%H-%M-%S).gpx
     /usr/bin/gpxlogger -r "$@" >/out/${filename} & loggerpid=`echo $!`
     echo "started /usr/bin/gpxlogger -r "$@", pid ${loggerpid}"
     sleep 300
